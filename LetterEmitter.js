907d81e812e79287064f071c38e4ba750df9be25
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-09 15:56:08 -0700

    failing test, trying to solve how to write test

--- LetterEmitter.js
+++ LetterEmitter.js
@@ -1,7 +1,12 @@
 const EventEmitter = require('events');
 
 class LetterEmitter extends EventEmitter {
-
+  read(str) {
+    str.split('').forEach(letter => {
+      this.emit('letter', letter);
+    });
+    this.emit('end');
+  }
 }
 
 module.exports = LetterEmitter;
--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -1,7 +1,19 @@
 const LetterEmitter = require('./LetterEmitter');
 
-describe('LetterEmitter', () => {
+describe('LetterEmitter', done => {
+  let letterEmitter;
+  beforeEach(done => {
+    letterEmitter = new LetterEmitter();
+    done();
+  });
   it('splits a string and emits an event for each letter', () => {
-
+    const letters = 'string string string';
+    letterEmitter.on('item', letter => {
+      expect(letters).toContainEqual(letter)
+    });
+    letterEmitter.on('end', () => {
+      done();
+    });
+    letterEmitter.read(letters);
   }); 
 });
